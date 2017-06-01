306df85525744ab406ec38f5ebd547706378eb45
kagowtham <ka.gow1996@gmail.com>
2017-06-01 15:13:14 +0530

    Update OddTwiceRepeated.java

--- OddTwiceRepeated.java
+++ OddTwiceRepeated.java
@@ -13,7 +13,7 @@ public class OddTwiceRepeated {
 		a[i]=s.nextInt();
 	Arrays.sort(a);
 	for(int i=0;i<n-1;i+=2){
-		System.out.println(a[i]+"=="+a[i+1]);
+		//System.out.println(a[i]+"=="+a[i+1]);
 		if(a[i]!=a[i+1]){
 			System.out.println("integer appear only once is "+a[i]);
 			break;
